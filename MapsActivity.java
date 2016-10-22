0419e4c6647b0e0807d8986e9963fada7362e0bf
JustWong <jjwong997@gmail.com>
2016-10-22 18:05:16 -0400

    Add files via upload

--- MapsActivity.java
+++ MapsActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -13,6 +14,9 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.location.LocationRequest;
@@ -33,12 +37,19 @@ public abstract class MapsActivity extends AppCompatActivity implements OnMapRea
     //private GoogleMap mMap;
     private GoogleApiClient mGoogleApiClient;
     private LocationRequest mLocationRequest;
+    private LocationListener ll;
     private Location mLastLocation;
     private LocationManager locationManager;
     private Location iDestination;
     private double iLat = iDestination.getLatitude();
     private double iLong = iDestination.getLongitude();
     private double limit;
+    private boolean trigger;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
 
     protected void onCreate() {
@@ -60,11 +71,20 @@ public abstract class MapsActivity extends AppCompatActivity implements OnMapRea
         }
     }
 
-    protected void onStart(){
+    protected void onStart() {
+        onCreate();
         mGoogleApiClient.connect();
         super.onStart();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        trigger = true;
+        createLocationRequest();
+        onConnect(ll);
     }
-    protected void onStop(){
+
+    protected void onStop() {
         mGoogleApiClient.disconnect();
         super.onStop();
     }
@@ -104,16 +124,17 @@ public abstract class MapsActivity extends AppCompatActivity implements OnMapRea
         LocationRequest mLocationRequest = new LocationRequest();
         mLocationRequest.setFastestInterval(3000);
         mLocationRequest.setMaxWaitTime(100000);
+        mLocationRequest.setInterval(5000 /* (long)checkDistance()*10 */);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
         //
         LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(mLocationRequest);
-        //PendingResult<LocationSettingsResult> result =
-        //        LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient, builder.build());
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient, builder.build());
     }
 
-    public void onConnect(Bundle ConnectionHint) {
-        if (true) {
+    public void onConnect(LocationListener ConnectionHint) {
+        if (trigger) {
             startLocationUpdates();
         }
         //mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
@@ -123,35 +144,67 @@ public abstract class MapsActivity extends AppCompatActivity implements OnMapRea
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
-        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        if (checkDistance() < limit)
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, ll);
+        //mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        //if (checkDistance() < limit)
         {
             //vibrate or something idk
         }
     }
 
-    public double checkDistance()
-    {
+    public void onLocationChanged(Location location) {
+        mLastLocation = location;
+        if (checkDistance() < limit) {
+            //do something
+        }
+        createLocationRequest();
+        onConnect(ll);
+    }
+
+    public double checkDistance() {
         double mLat = 0, mLong = 0;
         if (mLastLocation != null) {
             mLat = mLastLocation.getLatitude();
             mLong = mLastLocation.getLongitude();
         }
         int R = 6371; // Radius of the earth in km
-        double dLat = deg2rad(mLat-iLat);  // deg2rad below
-        double dLon = deg2rad(mLong-iLong);
+        double dLat = deg2rad(mLat - iLat);  // deg2rad below
+        double dLon = deg2rad(mLong - iLong);
         double a =
-                Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.sin(dLat / 2) * Math.sin(dLat / 2) +
                         Math.cos(deg2rad(iLat)) * Math.cos(deg2rad(mLat)) *
-                                Math.sin(dLon/2) * Math.sin(dLon/2)
-                ;
-        double  c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+                                Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
         double d = R * c; // Distance in km
-        return d/1000;
+        return d / 1000;
     }
 
     public double deg2rad(double deg) {
-        return deg * (Math.PI/180);
+        return deg * (Math.PI / 180);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Maps Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
     }
 }
