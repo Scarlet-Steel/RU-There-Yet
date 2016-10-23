27a5667523fcd2f2cc29bc277713e65a46f1bbd5
Hill Chang <Hillzc@users.noreply.github.com>
2016-10-22 20:24:48 -0400

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -9,6 +9,8 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
@@ -24,16 +26,18 @@ import android.widget.TextView;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 
-public abstract class MainActivity extends AppCompatActivity implements OnMapReadyCallback
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback
         , LocationListener, GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks{
 
     private GoogleApiClient mGoogleApiClient;
@@ -42,8 +46,8 @@ public abstract class MainActivity extends AppCompatActivity implements OnMapRea
     private Location mLastLocation;
     private LocationManager locationManager;
     private Location iDestination;
-    private double iLat = iDestination.getLatitude();
-    private double iLong = iDestination.getLongitude();
+    private double iLat = 74;//iDestination.getLatitude();
+    private double iLong = 74;//iDestination.getLongitude();
     private double limit;
     private boolean trigger;
 
@@ -134,20 +138,23 @@ public abstract class MainActivity extends AppCompatActivity implements OnMapRea
     @Override
     public void onStart() {
         super.onStart();
-
+        System.out.println("It ran");
         mGoogleApiClient.connect();
-
+        System.out.println("It ran2");
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            System.out.println("It ran3");
             return;
         }
         mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
         trigger = true;
+        checkDistance();
         createLocationRequest();
         onConnect(ll);
     }
 
     protected void createLocationRequest() {
         //Create location request
+        System.out.println("It 5ran");
         LocationRequest mLocationRequest = new LocationRequest();
         mLocationRequest.setFastestInterval(3000);
         mLocationRequest.setMaxWaitTime(100000);
@@ -172,7 +179,7 @@ public abstract class MainActivity extends AppCompatActivity implements OnMapRea
             return;
         }
 
-        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, (com.google.android.gms.location.LocationListener) this);
         //mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
         //if (checkDistance() < limit)
         {
@@ -187,11 +194,29 @@ public abstract class MainActivity extends AppCompatActivity implements OnMapRea
         }
         createLocationRequest();
         onConnect(ll);
+        System.out.println("It ran");
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
     }
 
     public double checkDistance() {
         double mLat = 0, mLong = 0;
+        System.out.println("It4 ran");
         if (mLastLocation != null) {
+            System.out.println("It 3ran");
             mLat = mLastLocation.getLatitude();
             mLong = mLastLocation.getLongitude();
         }
@@ -230,6 +255,26 @@ public abstract class MainActivity extends AppCompatActivity implements OnMapRea
 
         mGoogleApiClient.disconnect();
         super.onStop();
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
 
     }
 }
